f5eeb54843fda6ac088f68749399af6ae3481aee
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-04-09 18:45:28 +0900

    수정

--- component/DailyContents.js
+++ component/DailyContents.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './component/DailyContents';
+import './component/daily/DayOne';
+import './component/daily/DayTwo';
+import './component/daily/DayThree';
+import './component/daily/DayFour';
 
 function DailyContents() {
   return (
