bb9846b04d73dbe955118c1a9d0f24a0e6231eda
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-07-21 04:59:43 +0900

    New

--- src/index.js
+++ src/index.js
@@ -3,35 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      number:[
-        { id:0, title:'Main'},
-        { id:1, title:'Day One'},
-        { id:2, title:'Day Two' }
-      ]
-    }
-  }
   render(){
-    if(this.state.mode === 'main'){
-
-    }else if(this.state === ''){
-
-
-    }
     return(
-        <div className="App">
-            <NumberMenu data={this.props.number}></NumberMenu>
-            
-        </div>
-      );
+    <HeaderMenu></HeaderMenu>
+
+    )
   }
 }
 
+class HeaderMenu extends React.Component{
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
 
-export default App;
 
 
 ReactDOM.render(
@@ -40,6 +29,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
